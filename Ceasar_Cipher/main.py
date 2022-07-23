b746302d9bd3aa08c95936181b1198a4463c2e73
Punvireakroth <vireakrothpun@gmail.com>
2022-07-23 23:49:24 +0700

    decrypt the code

--- Ceasar_Cipher/main.py
+++ Ceasar_Cipher/main.py
@@ -24,4 +24,25 @@ def encrypt(text, shift):
     
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(text, shift)
+
+
+# -----------------------------Decryption --------------------------------
+#TODO-1: Create a function  'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(text, shift):
+
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+    old_text = ""
+    for j in text:
+        old_index = alphabet.index(j) - shift
+        old_text += alphabet[old_index]
+  #print output: "The decoded text is hello"
+    print(old_text)
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+
+decrypt(text, shift)
