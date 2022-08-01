029b05b23f2cb9902bbdc2b4e0362380b18e71d0
Punvireakroth <vireakrothpun@gmail.com>
2022-08-01 11:50:55 +0700

    attribute and method

--- OOP/main.py
+++ OOP/main.py
@@ -1,3 +1,14 @@
-import turtle
+from turtle import Turtle, Screen
 
-vireak = turtle.Turtle()
+vireak = Turtle()
+print(vireak)
+# Use method to the object 
+vireak.shape("turtle")
+
+
+my_screen = Screen()
+# attribute
+print(my_screen.canvheight)
+
+# method
+my_screen.exitonclick()
